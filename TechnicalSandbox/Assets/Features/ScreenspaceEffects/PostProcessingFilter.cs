e8d29645ce2e30a95716e1f38cc38116ad3ff678
Kristian Dixon <kristian.dixon@visr-vr.com>
2020-11-13 21:34:13 +0000

    Added fun shader which contains raymarched blooming spherers :)

---
+++ TechnicalSandbox/Assets/Features/ScreenspaceEffects/PostProcessingFilter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Effects/Raymarch (Generic)")]
+public class PostProcessingFilter : MonoBehaviour
+{
+    [SerializeField]
+    private Shader effectShader;
+
+    private Material _EffectMaterial;
+    private Camera currentCamera;
+
+    public int index = 0;
+
+    public Material EffectMaterial
+    {
+        get
+        {
+            if (!_EffectMaterial && effectShader)
+            {
+                _EffectMaterial = new Material(effectShader);
+                _EffectMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            return _EffectMaterial;
+        }
+    }
+
+    public Camera CurrentCamera
+    {
+        get
+        {
+            if (!currentCamera)
+            {
+                currentCamera = GetComponent<Camera>();
+                currentCamera.depthTextureMode = DepthTextureMode.Depth;
+            }
+            return currentCamera;
+        }
+    }
+
+   
+
+    [ImageEffectOpaque]
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!EffectMaterial)
+        {
+            //Do nothing
+            Graphics.Blit(source, destination);
+            return;
+        }
+        
+        //Note:: Could pass in view matrix instead
+        EffectMaterial.SetVector("_CameraPos", CurrentCamera.transform.position);
+        EffectMaterial.SetVector("_CameraFwd", CurrentCamera.transform.forward);
+        EffectMaterial.SetVector("_CameraUp", CurrentCamera.transform.up);
+
+        Graphics.Blit(source, destination, EffectMaterial);
+    }
+
+}
---
+++ TechnicalSandbox/Assets/Util/FlyCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float sensitivityHorizontal = 1, sensitivityVertical = 1;
+    public float movSpeed = 5f;
+
+    float xRotation, yRotation;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float up = Input.GetKey(KeyCode.Q) ? -1 : Input.GetKey(KeyCode.E) ? 1 : 0;
+
+        var movDir = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical") + transform.up * up;
+        
+        movDir = movDir.normalized;
+        transform.position += movDir * Time.deltaTime * movSpeed;
+        
+        xRotation += Input.GetAxis("Mouse X") * sensitivityHorizontal * Time.deltaTime;
+        yRotation += Input.GetAxis("Mouse Y") * sensitivityVertical * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(yRotation, xRotation, 0);
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
