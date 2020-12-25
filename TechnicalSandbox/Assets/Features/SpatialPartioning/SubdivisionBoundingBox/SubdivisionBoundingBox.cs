382c8f64b56e2dcbd9ac76932076df9a5f48a0fa
Kristian Dixon <kristian.dixon@visr-vr.com>
2020-12-25 22:11:18 +0000

    Did a bunch of thiings

--- TechnicalSandbox/Assets/Features/SpatialPartioning/SubdivisionBoundingBox/SubdivisionBoundingBox.cs
+++ TechnicalSandbox/Assets/Features/SpatialPartioning/SubdivisionBoundingBox/SubdivisionBoundingBox.cs
@@ -2,27 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SubdivisionBoundingBox : MonoBehaviour
+public class Node
 {
-    public Mesh mesh;
+    public List<Node> subNodes = new List<Node>();
+    public bool isLeaf = false;
+    public Bounds bounds;
+} 
 
-    public int depthLimit = 4;
-    List<Bounds> bounds;
-
+public class Octtree 
+{
+    Node root;
     Vector3[] vertices;
 
-    void CreateBoundingBoxes()
+    Bounds rootBounds;
+
+    int depthLimit;
+
+    public Octtree(Bounds bounds, Mesh mesh, int depthLimit)
     {
-        if (mesh == null) return;
-        var originalBounds = mesh.bounds;
-        bounds = new List<Bounds>();
+        rootBounds = bounds;
         vertices = mesh.vertices;
-        bounds.Add(originalBounds);
-        A(originalBounds, 0);
+        this.depthLimit = depthLimit;
+        Generate();
     }
 
-    void A(Bounds currentBounds, int depth)
+    void Generate()
     {
+        root = new Node();
+        Subdivide(root, rootBounds, 0);
+    }
+
+    bool Subdivide(Node currentNode, Bounds currentBounds, int depth)
+    {
+        currentNode.bounds = currentBounds;
         bool areVerticiesPresentInBounds = false;
         for (int i = 0; i < vertices.Length; i++)
         {
@@ -35,61 +47,180 @@ public class SubdivisionBoundingBox : MonoBehaviour
 
         if (!areVerticiesPresentInBounds)
         {
-            return;
+            return false;
         }
 
         if (depth == depthLimit)
         {
-            bounds.Add(currentBounds);
-            return;
+            currentNode.isLeaf = true;
+            return true;
         }
 
-        
-
+        bool retVal = false;
         if (areVerticiesPresentInBounds)
         {
             //Split into multiple bounds
             Bounds subBounds = new Bounds(currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(1, 1, 1)) * 0.5f, currentBounds.extents);
-            A(subBounds, depth + 1);
-            
-            subBounds = new Bounds(currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(-1, 1, 1)) * 0.5f, currentBounds.extents);
-            A(subBounds, depth + 1);
+            {
+                var node = new Node();
+                if (Subdivide(node, subBounds, depth + 1))
+                {
+                    retVal = true;
+                }
+                currentNode.subNodes.Add(node);
 
-            subBounds = new Bounds(currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(1, -1, 1)) * 0.5f, currentBounds.extents);
-            A(subBounds, depth + 1);
+            }
 
-            subBounds = new Bounds(currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(1, 1, -1)) * 0.5f, currentBounds.extents);
-            A(subBounds, depth + 1);
+            subBounds.center = currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(-1, 1, 1)) * 0.5f;
+            {
+                var node = new Node();
+                if (Subdivide(node, subBounds, depth + 1))
+                {
+                    retVal = true;
 
-            subBounds = new Bounds(currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(-1, -1, 1)) * 0.5f, currentBounds.extents);
-            A(subBounds, depth + 1);
+                }
+                currentNode.subNodes.Add(node);
 
-            subBounds = new Bounds(currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(-1, -1, -1)) * 0.5f, currentBounds.extents);
-            A(subBounds, depth + 1);
+            }
 
-            subBounds = new Bounds(currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(-1, 1, -1)) * 0.5f, currentBounds.extents);
-            A(subBounds, depth + 1);
+            subBounds.center = currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(1, -1, 1)) * 0.5f;
+            {
+                var node = new Node();
+                if (Subdivide(node, subBounds, depth + 1))
+                {
+                    retVal = true;
 
-            subBounds = new Bounds(currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(1, -1, -1)) * 0.5f, currentBounds.extents);
-            A(subBounds, depth + 1);
+
+                }
+                currentNode.subNodes.Add(node);
+
+            }
+
+            subBounds.center = currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(1, 1, -1)) * 0.5f;
+            {
+                var node = new Node();
+                if (Subdivide(node, subBounds, depth + 1))
+                {
+                    retVal = true;
+
+                }
+                currentNode.subNodes.Add(node);
+
+            }
+
+            subBounds.center = currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(-1, -1, 1)) * 0.5f;
+            {
+                var node = new Node();
+                if (Subdivide(node, subBounds, depth + 1))
+                {
+                    retVal = true;
+
+                }
+                currentNode.subNodes.Add(node);
+
+            }
+
+            subBounds.center = currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(-1, -1, -1)) * 0.5f;
+            {
+                var node = new Node();
+                if (Subdivide(node, subBounds, depth + 1))
+                {
+                    retVal = true;
+
+                }
+                currentNode.subNodes.Add(node);
+
+            }
+
+            subBounds.center = currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(-1, 1, -1)) * 0.5f;
+            {
+                var node = new Node();
+                if (Subdivide(node, subBounds, depth + 1))
+                {
+                    retVal = true;
+
+                }
+                currentNode.subNodes.Add(node);
+
+            }
+
+            subBounds.center = currentBounds.center - Vector3.Scale(currentBounds.extents, new Vector3(1, -1, -1)) * 0.5f;
+            {
+                var node = new Node();
+                if (Subdivide(node, subBounds, depth + 1))
+                {
+                    retVal = true;
+
+                }
+                currentNode.subNodes.Add(node);
+
+            }
         }
+
+        return retVal;
     }
 
-    // Start is called before the first frame update
+    public void DrawGizmos()
+    {
+        DrawRecursively(root);
+    }
+
+    //Returns true when bottommost child is true.
+    bool DrawRecursively(Node node)
+    {
+        bool drawWithColour = false;
+        for(int i = 0; i < node.subNodes.Count; i++)
+        {
+            drawWithColour |= DrawRecursively(node.subNodes[i]);
+        }
+
+        if(node.subNodes.Count == 0)
+        {
+            drawWithColour = node.isLeaf;
+        }
+
+        Gizmos.color = drawWithColour ? Color.green : Color.white;
+        Gizmos.DrawWireCube( node.bounds.center, node.bounds.size);
+        return drawWithColour;
+    }
+
+}
+
+public class SubdivisionBoundingBox : MonoBehaviour
+{
+    public Mesh mesh;
+
+    public int depthLimit = 4;
+
+    Vector3[] vertices;
+
+    Octtree tree = null;
+
     void Start()
     {
-
         CreateBoundingBoxes();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void CreateBoundingBoxes()
     {
-        
+        if (mesh == null) return;
+        var originalBounds = mesh.bounds;
+
+        float max = Mathf.Max(Mathf.Max(originalBounds.size.x, originalBounds.size.y), originalBounds.size.z);
+
+        originalBounds.size = Vector3.one * max;
+
+        tree = new Octtree(originalBounds, mesh, 4);
     }
 
     private void OnDrawGizmos()
     {
+        if (tree != null)
+        {
+            tree.DrawGizmos();
+        }
+
         /*Bounds origin = new Bounds(Vector3.zero, new Vector3(20, 25, 7));
 
         Gizmos.DrawWireCube(origin.center, origin.size);
@@ -102,10 +233,10 @@ public class SubdivisionBoundingBox : MonoBehaviour
         }
         */
 
-        for(int i = 0; i < bounds.Count; i++)
+        /*for(int i = 0; i < bounds.Count; i++)
         {
             Bounds subBounds = bounds[i];
             Gizmos.DrawWireCube(transform.TransformPoint(subBounds.center), Vector3.Scale(transform.localScale, subBounds.size));
-        }
+        }*/
     }
 }
